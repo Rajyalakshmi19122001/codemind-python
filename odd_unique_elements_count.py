fbff8bf1dbc20aceaea3338e635b94d93f83311a
Rajyalakshmi19122001 <102218285+Rajyalakshmi19122001@users.noreply.github.com>
2022-05-25 20:32:45 +0530

    N=int(input()) l=list(map(int,input().split())) d=[] e=[] c=0 for i in l:   if i in d:       continue   d.append(i) for j in d:   if j%2!=0:       e.append(j) print(len(e))

---
+++ odd_unique_elements_count.py
@@ -0,0 +1,13 @@
+N=int(input())
+l=list(map(int,input().split()))
+d=[]
+e=[]
+c=0
+for i in l:
+  if i in d:
+      continue
+  d.append(i)
+for j in d:
+  if j%2!=0:
+      e.append(j)
+print(len(e))
